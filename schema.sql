cda71e46ae540160a4b64e425fa7f06f64391215
rubadhanaraj <rubadhanaraj@outlook.com>
2022-08-10 00:56:53 -0400

    Adding schema sql file

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+DROP TABLE titles CASCADE;
+
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
 	 dept_no VARCHAR(4) NOT NULL,
@@ -36,8 +38,7 @@ CREATE TABLE titles (
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
@@ -48,4 +49,10 @@ CREATE TABLE dept_emp (
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
   PRIMARY KEY (emp_no,dept_no)
 );
+
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries ;
+SELECT * FROM titles;
+SELECT * FROM dept_emp;
